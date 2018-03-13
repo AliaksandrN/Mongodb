1669335f401d75d9887c4674fb048643c187774b
AliaksandrN <novik.alexander.88@gmail.com>
2018-03-13 21:21:52 +0100

    conf_5.js w:2, j:true

---
+++ bin/js/conf_5.js
@@ -0,0 +1,13 @@
+rsconf4 = {
+  _id: "replicaSet",
+  members: [
+    { _id: 0, host: ip1 },
+    { _id: 1, host: ip2 },
+    { _id: 2, host: ip3 }
+   ],
+  settings: {
+    getLastErrorDefaults : { w: 2, j: true }
+  }
+}
+
+rs.reconfig(rsconf4)
